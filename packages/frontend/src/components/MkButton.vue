822ac8e1a016a17db948e99aeafb1427cc45e8cf
Kagami Sascha Rosylight <saschanaz@outlook.com>
2023-04-02 10:41:43 +0200

    tmp

--- packages/frontend/src/components/MkButton.vue
+++ packages/frontend/src/components/MkButton.vue
@@ -5,6 +5,7 @@
 	:class="[$style.root, { [$style.inline]: inline, [$style.primary]: primary, [$style.gradate]: gradate, [$style.danger]: danger, [$style.rounded]: rounded, [$style.full]: full, [$style.small]: small, [$style.large]: large, [$style.asLike]: asLike }]"
 	:type="type"
 	:name="name"
+	:value="value"
 	@click="emit('click', $event)"
 	@mousedown="onMousedown"
 >
@@ -47,6 +48,7 @@ const props = defineProps<{
 	large?: boolean;
 	asLike?: boolean;
 	name?: string;
+	value?: string;
 }>();
 
 const emit = defineEmits<{
